9c05c43b6cdbabeab9b1034d1a018edd87d3627b
JoaoPedro07 <37756452+JoaoPedro07@users.noreply.github.com>
2019-07-27 20:33:54 -0300

    Update baixarPOO.py

--- baixarPOO.py
+++ baixarPOO.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+import youtube_dl
 from tkinter import Frame, Button, END, Tk, RIGHT, Label, LEFT, BOTTOM
 from tkinter.scrolledtext import ScrolledText
 from selenium import webdriver 
@@ -8,36 +10,26 @@ class App:
      def __init__(self, master=None):
              self.root = master
              self.root.title("Musicas")
+             self.root.geometry("230x260")
 
-             self.frame_msg = Frame(self.root)
-             self.frame_msg["pady"] = 20
-             self.frame_msg["padx"] = 87
-             self.frame_msg.pack()
+             self.frame = Frame(self.root)
 
-             self.frame_scroll = Frame(self.root)
-             self.frame_scroll["padx"] = 5
-             self.frame_scroll["pady"] = 10
-             self.frame_scroll.pack()
-             
-             self.frame_button = Frame(self.root)
-             self.frame_button["padx"] = 5
-             self.frame_button["pady"] = 5
-             self.frame_button.pack(side=RIGHT)
+             self.msg = Label(self.frame, text="Insira as músicas que \ndeseja baixar no \ncampo abaixo")
+             self.msg["font"] = ("Arial", "10")
+             self.msg.grid(row=1, column=1)
 
-             self.msg = Label(self.frame_msg, text="Insira as músicas que deseja baixar no campo abaixo")
-             self.msg["font"] = ("Arial", "14")
-             self.msg.pack(side=LEFT)
+             self.scrolled = ScrolledText(self.frame, width=25, height=10)
+             self.scrolled.grid(row=2, column=1)
 
-             self.scrolled = ScrolledText(self.frame_scroll)
-             self.scrolled.pack()
-
-             self.button = Button(self.frame_button)
-             self.button["text"] = "Ok"
+             self.button = Button(self.frame)
+             self.button["text"] = "Enviar"
              self.button["command"] = self.sair
-             self.button["font"] = ("Arial", "12", "bold")
-             self.button["width"] = 8
+             self.button["font"] = ("Arial", "12")
+             self.button["width"] = 5
              self.button["height"] = 1 
-             self.button.pack(side=RIGHT)
+             self.button.grid(row=3, column=1)
+
+             self.frame.pack()
 
      def sair(self):
              with open("musicas.txt", "w") as file:
@@ -47,38 +39,34 @@ class App:
 class Automacao():
     
     def __init__(self):
-        self.driver = webdriver.Chrome(executable_path="C:/chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path="chromedriver.exe")
     
     def BuscarMusica(self, nome):
         self.nome = nome
         self.driver.get("https://www.youtube.com/results?search_query={}".format(self.nome))
-        video = self.driver.find_elements_by_tag_name("ytd-video-renderer")
-        tentativa = 0
-        while True:
-            try:
-                video[tentativa].click()
-                break
-            except:
-                tentativa +=1
-
+        time.sleep(1)
+        video_div = self.driver.find_elements_by_id("contents")
+        time.sleep(1)
+        video_div[0].find_elements_by_id("dismissable")[0].click()
         time.sleep(2)
         return self.driver.current_url
 
     def BaixarMusica(self, url):
-        self.driver.get("https://2conv.com/pt/")
-        search = self.driver.find_element_by_class_name("url-conv")
-        search.send_keys(url)
-        button_div = self.driver.find_element_by_class_name("button-group")
-        button = button_div.find_element_by_class_name("button")
-        button.click()
-        while True:
-            try:
-                download_button_div = self.driver.find_element_by_class_name("text-center")
-                download_button = download_button_div.find_element_by_class_name("button")
-                download_button.click()
-                break
-            except:
-                pass
+        self.url = url
+        print(self.nome)
+        outtmpl = self.nome + '.mp3'
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': outtmpl,
+            'postprocessors': [
+                {'key': 'FFmpegExtractAudio','preferredcodec': 'mp3',
+                'preferredquality': '192',
+                },
+            ],
+        }
+
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([str(self.url)])
 
     def Quit(self):
         self.driver.quit()
@@ -93,6 +81,7 @@ if __name__ == "__main__":
         musicas = file.readlines()
 
     for music in musicas:
-        url = auto.BuscarMusica(nome=music)
+        print(music)
+        url = auto.BuscarMusica(nome=music.strip("'\n'"))
         auto.BaixarMusica(url=url)
     auto.Quit()
